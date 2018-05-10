c8a31139e77b228bc1435d39032b9b8e696d0399
André Hashiguchi <alshashiguchi@gmail.com>
2018-05-09 22:33:20 -0300

    Operadores Ternáro

---
+++ fundamentos/naoTernario/naoTernario.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Não tem operador ternario
+func obterResultado(nota float64) string {
+	if nota >= 6 {
+		return "Aprovado"
+	}
+	return "Reprovado"
+}
+
+func main() {
+	fmt.Println(obterResultado(6.2))
+}
