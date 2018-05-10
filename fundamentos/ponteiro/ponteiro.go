4d779fbb6b9e0e5b791a8f68bc735d3d76ca62ca
André Hashiguchi <alshashiguchi@gmail.com>
2018-05-09 22:33:27 -0300

    Ponteiros

---
+++ fundamentos/ponteiro/ponteiro.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 1
+
+	var p *int = nil
+	p = &i // pegando o endereço da variavél
+	*p++   // desreferenciando (pegando o valor)
+	i++
+
+	// Go não tem aritmética de ponteiros
+	// p++
+
+	fmt.Println(p, *p, i, &i)
+}
