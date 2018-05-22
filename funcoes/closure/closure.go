136879dd687add866c37c20759f79e40ea48c700
André Hashiguchi <alshashiguchi@gmail.com>
2018-05-22 13:04:36 -0300

    Conceito de Closure

---
+++ funcoes/closure/closure.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+// o retorno é outra funcao
+func closure() func() {
+	x := 10
+	var funcao = func() {
+		fmt.Println(x)
+	}
+	return funcao
+}
+
+func main() {
+	x := 20
+	fmt.Println(x)
+
+	imprimeX := closure()
+	imprimeX()
+}
