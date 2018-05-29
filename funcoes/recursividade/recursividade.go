7b9567da6422e48f2c35c144549f4a6db19f1cda
André Hashiguchi <alshashiguchi@gmail.com>
2018-05-28 22:28:32 -0300

    Recursividade

---
+++ funcoes/recursividade/recursividade.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func fatorial(n int) (int, error) {
+	switch {
+	case n < 0:
+		return -1, fmt.Errorf("número inválido: %d", n)
+
+	case n == 0:
+		return 1, nil
+
+	default:
+		fatorialAnterior, _ := fatorial(n - 1)
+		return n * fatorialAnterior, nil
+	}
+}
+
+func main() {
+	resultado, _ := fatorial(5)
+	fmt.Println(resultado)
+
+	_, erro := fatorial(-5)
+
+	if erro != nil {
+		fmt.Println(erro)
+	}
+}
