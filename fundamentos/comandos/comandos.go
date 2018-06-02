26adf37ce7a983fda1259b566730a8a55a7e45a5
André Hashiguchi <alshashiguchi@gmail.com>
2018-06-01 22:40:59 -0300

    Comandos do go

--- fundamentos/comandos/comandos.go
+++ fundamentos/comandos/comandos.go
@@ -7,8 +7,3 @@ import (
 func main() {
 	fmt.Printf("Outro programa em %s!\n", "Go")
 }
-
-// go vet - analisa o código
-// go build - compila o programa (./comandos)
-// go run - compila e executa
-// go get - instala pacotes
