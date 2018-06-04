4fe5ad14889ad1f0fbdc719789a6d6cba84c8f54
André Hashiguchi <alshashiguchi@gmail.com>
2018-06-03 22:24:36 -0300

    Usando Goroutine e Channel

---
+++ concorrencia/channelegoroutine/channel.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// Channel (canal) - é a forma de comunicação entre goroutines (Ponto de sincronismo da goroutine)
+// channel é um tipo como qualquer outro tipo como int, string etc...
+
+func doisTresQuatroVezes(base int, c chan int) {
+	time.Sleep(time.Second)
+	c <- 2 * base // enviando dados para o canal
+
+	time.Sleep(time.Second)
+	c <- 3 * base
+
+	time.Sleep(3 * time.Second)
+	c <- 4 * base
+}
+
+func main() {
+	c := make(chan int)
+	go doisTresQuatroVezes(2, c)
+
+	a, b := <-c, <-c // recebendo dados do canal
+
+	fmt.Println(a, b)
+
+	fmt.Println(<-c)
+}
